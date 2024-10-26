7e2a0e24c4863f8e5cd0d72781e793ba0a2e0cef
pablo <londro23@hotmail.com>
2024-10-26 13:21:14 +0200

    cambios

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://pablo411.github.io/react-rick-aand-morty",
+  base: "https://pablo411.github.io/react-rick-aand-morty/",
   plugins: [react()],
 })
